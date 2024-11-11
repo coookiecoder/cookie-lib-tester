514acdd9d791f746793198f82a8b722c8358f24b
abareux <abareux@student.42mulhouse.fr>
2024-11-11 19:38:54 +0000

    all the test for the string sub module done

--- loader.py
+++ loader.py
@@ -3,12 +3,65 @@ from ctypes import *
 cookie_lib = CDLL("./cookie-lib/libcookie_lib.so")
 cookie_lib.cookie_put_char.argtypes = [c_char]
 cookie_lib.cookie_put_char.restype = c_uint64
+cookie_lib.cookie_put_char_nl.argtypes = [c_char]
+cookie_lib.cookie_put_char_nl.restype = c_uint64
+cookie_lib.cookie_put_char_fd.argtypes = [c_char, c_int]
+cookie_lib.cookie_put_char_fd.restype = c_uint64
+cookie_lib.cookie_put_char_fd_nl.argtypes = [c_char, c_int]
+cookie_lib.cookie_put_char_fd_nl.restype = c_uint64
+
+cookie_lib.cookie_put_str.argtypes = [c_char_p]
+cookie_lib.cookie_put_str.restype = c_uint64
+cookie_lib.cookie_put_str_nl.argtypes = [c_char_p]
+cookie_lib.cookie_put_str_nl.restype = c_uint64
+cookie_lib.cookie_put_str_fd.argtypes = [c_char_p, c_int]
+cookie_lib.cookie_put_str_fd.restype = c_uint64
+cookie_lib.cookie_put_str_fd_nl.argtypes = [c_char_p, c_int]
+cookie_lib.cookie_put_str_fd_nl.restype = c_uint64
+
+cookie_lib.cookie_strcmp.argtypes = [c_char_p, c_char_p]
+cookie_lib.cookie_strcmp.restype = c_uint64
+cookie_lib.cookie_strcmp_n.argtypes = [c_char_p, c_char_p, c_uint64]
+cookie_lib.cookie_strcmp_n.restype = c_uint64
+
+cookie_lib.cookie_str_dup.argtypes = [c_char_p]
+cookie_lib.cookie_str_dup.restype = c_char_p
 
 cookie_lib.cookie_strlen.argtypes = [c_char_p]
 cookie_lib.cookie_strlen.restype = c_uint64
 
 def putchar(c):
     return cookie_lib.cookie_put_char(c.encode("utf-8"))
+
+def putchar_nl(c):
+    return cookie_lib.cookie_put_char_nl(c.encode("utf-8"))
+
+def putchar_fd(c, fd):
+    return cookie_lib.cookie_put_char(c.encode("utf-8"), fd)
+
+def putchar_fd_nl(c, fd):
+    return cookie_lib.cookie_put_char_nl(c.encode("utf-8"), fd)
+
+def putstr(s):
+    return cookie_lib.cookie_put_str(s.encode("utf-8"))
+
+def putstr_nl(s):
+    return cookie_lib.cookie_put_str_nl(s.encode("utf-8"))
+
+def putstr_fd(s, fd):
+    return cookie_lib.cookie_put_str_fd(s.encode("utf-8"), fd)
+
+def putstr_fd_nl(s, fd):
+    return cookie_lib.cookie_put_str_fd_nl(s.encode("utf-8"), fd)
+
+def strcmp(s1, s2):
+    return cookie_lib.cookie_strcmp(s1.encode("utf-8"), s2.encode("utf-8"))
+
+def strcmp_n(s1, s2, n):
+    return cookie_lib.cookie_strcmp(s1.encode("utf-8"), s2.encode("utf-8"), n)
+
+def strdup(s):
+    return cookie_lib.cookie_str_dup(s.encode("utf-8"))
 
 def strlen(s):
     return cookie_lib.cookie_strlen(s.encode("utf-8"))
--- main.py
+++ main.py
@@ -1,23 +1,79 @@
 import subprocess
+import unittest
 
 subprocess.run(['make', 'libcookie_lib.so', '-C', 'cookie-lib'])
 
-import unittest
-
 from loader import *
 
-class MainTest(unittest.TestCase):
-    def test_putchar(self):
+class StringTest(unittest.TestCase):
+    def test_put_char(self):
         array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         for char in array:
             self.assertEqual(putchar(char), 1)
-    def test_strlen(self):
+
+    def test_put_char_new_line(self):
+        array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        for char in array:
+            self.assertEqual(putchar_nl(char), 2)
+
+    def test_put_char_file_descriptor(self):
+        array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        for char in array:
+            self.assertEqual(putchar_fd(char, 1), 1)
+
+    def test_put_char_new_line_file_descriptor(self):
+        array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        for char in array:
+            self.assertEqual(putchar_fd_nl(char, 1), 2)
+
+    def test_put_string(self):
+        array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        for char in array:
+            self.assertEqual(putstr(char), 1)
+        array = ["test", "", "eoieurtowpeiutwer", "dfhdfg", "dfglhkjdfglkhjdflkhjdflhkjfgh"]
+        for string in array:
+            self.assertEqual(putstr(string), string.__len__())
+
+    def test_put_string_new_line(self):
+        array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        for char in array:
+            self.assertEqual(putstr_nl(char), 2)
+        array = ["test", "", "eoieurtowpeiutwer", "dfhdfg", "dfglhkjdfglkhjdflkhjdflhkjfgh"]
+        for string in array:
+            self.assertEqual(putstr_nl(string), string.__len__() + 1)
+
+    def test_put_string_file_descriptor(self):
+        array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        for char in array:
+            self.assertEqual(putstr_fd(char, 1), 1)
+        array = ["test", "", "eoieurtowpeiutwer", "dfhdfg", "dfglhkjdfglkhjdflkhjdflhkjfgh"]
+        for string in array:
+            self.assertEqual(putstr_fd(string, 1), string.__len__())
+
+    def test_put_string_new_line_file_descriptor(self):
+        array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        for char in array:
+            self.assertEqual(putstr_fd_nl(char, 1), 2)
+        array = ["test", "", "eoieurtowpeiutwer", "dfhdfg", "dfglhkjdfglkhjdflkhjdflhkjfgh"]
+        for string in array:
+            self.assertEqual(putstr_fd_nl(string, 1), string.__len__() + 1)
+
+    def test_string_compare(self):
+        self.assertEqual(strcmp("test", "test"), 0)
+        self.assertEqual(strcmp("test", "tese"), 1)
+
+    def test_string_compare_n(self):
+        self.assertEqual(strcmp_n("test", "test", 4), 0)
+        self.assertEqual(strcmp_n("test", "tese", 4), 1)
+        self.assertEqual(strcmp_n("test", "tese", 3), 1)
+
+    def test_string_duplicate(self):
+        self.assertEqual(strdup("test"), b"test")
+
+    def test_string_len(self):
         array = ["test", "", "eoieurtowpeiutwer", "dfhdfg", "dfglhkjdfglkhjdflkhjdflhkjfgh"]
         for string in array:
             self.assertEqual(strlen(string), string.__len__())
-            print("string : " + string)
-            print("strlen : " + str(strlen(string)))
-
 
 
 if __name__ == '__main__':
